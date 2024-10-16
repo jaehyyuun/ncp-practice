52eab5a7fe787d9566be7edc1719f8c71a8b539c
jaehyyuun <jaehyunj393@gmail.com>
2024-10-16 15:09:41 +0900

    chore: mysql 파드 배포

--- src/main/java/com/example/ncp_practice/NcpPracticeApplication.java
+++ src/main/java/com/example/ncp_practice/NcpPracticeApplication.java
@@ -1,7 +1,13 @@
 package com.example.ncp_practice;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class NcpPracticeApplication {
@@ -11,3 +17,22 @@ public class NcpPracticeApplication {
 	}
 
 }
+/*@SpringBootApplication
+public class NcpPracticeApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcpPracticeApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Testing MySQL connection...");
+		try (Connection connection = dataSource.getConnection()) {
+			System.out.println("Connected to MySQL: " + connection.getMetaData().getDatabaseProductName());
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}*/
